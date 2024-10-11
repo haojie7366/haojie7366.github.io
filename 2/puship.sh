3e32bc10efb4cab5c8fdac648691aa670dfe4c08
haojie7366 <haojie7366@gmail.com>
2024-10-11 22:54:44 +0800

    Fri Oct 11 10:54:43 PM CST 2024

--- 2/puship.sh
+++ 2/puship.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd haojie7366.github.io
 echo $1:$2 >2/ipnat.txt
-./push.sh
+#./push
+
--- ssh/puship2.sh
+++ ssh/puship2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd haojie7366.github.io
 echo "$1 -p $2 -o StrictHostKeyChecking=no" >ssh/2
-./push.sh
+#./push.sh
