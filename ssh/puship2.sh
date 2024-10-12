7c651b7222a49256232dbf0cf45e200b751fab01
haojie7366 <haojie7366@gmail.com>
2024-10-12 10:22:26 +0800

    Sat Oct 12 10:22:26 AM CST 2024

--- ssh/puship2.sh
+++ ssh/puship2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd haojie7366.github.io
 echo "$1 -p $2 -o StrictHostKeyChecking=no" >ssh/2
+echo "$1 -p $2 -o StrictHostKeyChecking=no" >ssh/2.txt
 #./push.sh
--- ssh/puship3.sh
+++ ssh/puship3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd haojie7366.github.io
-echo "$1 -p $2 -o StrictHostKeyChecking=no" >ssh/3
+echo "$1 -p $2 -o StrictHostKeyChecking=no" >ssh/3.txt
 #./push.sh
