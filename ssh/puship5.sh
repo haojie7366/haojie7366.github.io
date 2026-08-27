36871fb056e19ab46c4c23b7b38109b3270b05b0
haojie7366 <haojie7366@gmail.com>
2026-08-28 02:26:08 +0800

    Fri Aug 28 02:26:08 AM CST 2026

--- ssh/puship5.sh
+++ ssh/puship5.sh
@@ -4,4 +4,7 @@ cd haojie7366.github.io
 #sudo iptables -t nat -A OUTPUT -d $1 -p tcp --dport $2 -j DNAT --to-destination 127.0.0.1:9443
 echo "$1:$2" >ssh/5
 echo "$1:$2" >ssh/5.txt
+sleep 30
+scp ssh/5 me@192.168.1.3/haojie7366.github.io/ssh
+scp ssh/5.txt me@192.168.1.3/haojie7366.github.io/ssh
 #./push.sh
